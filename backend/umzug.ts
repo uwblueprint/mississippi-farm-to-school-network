1e86411fe5b281f19de1aaf55c70372cdf5c9528
tonyqiu123 <tonyqiu12345@gmail.com>
2026-02-08 00:00:15 -0500

    fix lint

--- backend/umzug.ts
+++ backend/umzug.ts
@@ -5,8 +5,7 @@ import { Sequelize } from 'sequelize-typescript';
 
 const DATABASE_URL =
   process.env.NODE_ENV === 'production'
-    ?  
-      process.env.DATABASE_URL!
+    ? process.env.DATABASE_URL!
     : `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.DB_HOST}:5432/${process.env.POSTGRES_DB_DEV}`;
 
 const sequelize = new Sequelize(DATABASE_URL, {
