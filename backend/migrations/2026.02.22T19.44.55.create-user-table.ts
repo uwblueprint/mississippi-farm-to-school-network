c21a61b6cda68f19e74a5d6404396024b914bcd3
Vidu <vidsterbroyo@gmail.com>
2026-02-22 15:05:04 -0500

    Adds fields to user migration

--- backend/migrations/2026.02.22T19.44.55.create-user-table.ts
+++ backend/migrations/2026.02.22T19.44.55.create-user-table.ts
@@ -1,6 +1,6 @@
 import type { MigrationFn } from 'umzug/lib/types';
 import type { Sequelize } from 'sequelize-typescript';
-import { DataTypes } from 'sequelize';
+import { DataTypes, Sequelize as SequelizeStatic } from 'sequelize';
 
 export const up: MigrationFn = async (params) => {
   const sequelize = params.context as Sequelize;
@@ -8,19 +8,39 @@ export const up: MigrationFn = async (params) => {
 
   await queryInterface.createTable('users', {
     id: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        defaultValue: DataTypes.UUIDV4,
-        allowNull: false,
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
     },
     firebase_uid: { 
-        type: DataTypes.STRING, 
-        unique: true,
-        allowNull: false 
+      type: DataTypes.STRING, 
+      unique: true,
+      allowNull: false 
     },
     role: {
-        type: DataTypes.ENUM,
-        allowNull: false,
+      type: DataTypes.ENUM('ADMIN', 'FARMER'),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    is_verified: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: SequelizeStatic.fn('now'),
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: SequelizeStatic.fn('now'), 
     },
   });
 };
