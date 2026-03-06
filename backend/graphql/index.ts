ca316ad58f076e4e8981817b29b2c704cdaa3beb
Erica Han <57574635+ericahan22@users.noreply.github.com>
2026-03-05 21:52:00 -0500

    Implement Firebase file storage service with GraphQL integration (#45)

--- backend/graphql/index.ts
+++ backend/graphql/index.ts
@@ -4,13 +4,15 @@ import merge from 'lodash/merge';
 import emailResolvers from '@/graphql/resolvers/emailResolvers';
 import userResolvers from '@/graphql/resolvers/userResolvers';
 import authResolvers from '@/graphql/resolvers/authResolvers';
+import fileStorageResolvers from '@/graphql/resolvers/fileStorageResolvers';
 import emailType from '@/graphql/types/emailType';
 import userType from '@/graphql/types/userType';
 import authType from '@/graphql/types/authType';
+import fileStorageType from '@/graphql/types/fileStorageType';
 
 const executableSchema = makeExecutableSchema({
-  typeDefs: [emailType, userType, authType],
-  resolvers: merge(emailResolvers, userResolvers, authResolvers),
+  typeDefs: [emailType, userType, authType, fileStorageType],
+  resolvers: merge(emailResolvers, userResolvers, authResolvers, fileStorageResolvers),
 });
 
 export default executableSchema;
