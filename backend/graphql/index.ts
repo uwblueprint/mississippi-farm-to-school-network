44c3c47a1d56f130d8581c1ea8648a6dff0dca05
Lukas Li <lukas.li.album@gmail.com>
2026-03-09 19:57:55 -0400

    register farmResolvers in GraphQL schema

--- backend/graphql/index.ts
+++ backend/graphql/index.ts
@@ -4,6 +4,7 @@ import merge from 'lodash/merge';
 import emailResolvers from '@/graphql/resolvers/emailResolvers';
 import userResolvers from '@/graphql/resolvers/userResolvers';
 import authResolvers from '@/graphql/resolvers/authResolvers';
+import farmResolvers from '@/graphql/resolvers/farmResolvers';
 import fileStorageResolvers from '@/graphql/resolvers/fileStorageResolvers';
 import emailType from '@/graphql/types/emailType';
 import userType from '@/graphql/types/userType';
@@ -13,7 +14,7 @@ import fileStorageType from '@/graphql/types/fileStorageType';
 
 const executableSchema = makeExecutableSchema({
   typeDefs: [emailType, userType, authType, farmType, fileStorageType],
-  resolvers: merge(emailResolvers, userResolvers, authResolvers, fileStorageResolvers),
+  resolvers: merge(emailResolvers, userResolvers, authResolvers, farmResolvers, fileStorageResolvers),
 });
 
 export default executableSchema;
