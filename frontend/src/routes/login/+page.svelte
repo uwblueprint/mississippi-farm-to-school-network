7b143bc3c071305bf64276732b1b1ba20d1001ce
Jesse Huang <jesshuang5733@gmail.com>
2026-06-04 19:12:02 -0400

    ran linter

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -4,7 +4,7 @@
 
 <section class="space-y-4 p-6 md:p-10">
 	<h1 class="text-3xl font-bold text-surface-950-50">LOGIN PAGE</h1>
-    <div class="flex flex-wrap gap-3">
+	<div class="flex flex-wrap gap-3">
 		<a class="btn preset-filled-primary-500" href="/farmer">am farmer</a>
 		<a class="btn preset-filled-primary-500" href="/admin">am admin</a>
 		<a class="btn preset-outlined-surface-300-700" href="/">jk go home</a>
