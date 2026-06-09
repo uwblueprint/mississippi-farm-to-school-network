c03d5e9876e16191ef04bdbfb7d1d607716ba52a
Lukas Li <--global>
2026-06-08 23:55:39 -0400

    Refactor migration to enforce non-null home_county by changing column definition and removing initial null update

--- backend/migrations/2026.06.08T12.00.00.add-farm-home-county.ts
+++ backend/migrations/2026.06.08T12.00.00.add-farm-home-county.ts
@@ -6,15 +6,6 @@ export const up: MigrationFn = async (params) => {
   const sequelize = params.context as Sequelize;
   const queryInterface = sequelize.getQueryInterface();
 
-  await queryInterface.addColumn('farms', 'home_county', {
-    type: DataTypes.STRING,
-    allowNull: true,
-  });
-
-  await queryInterface.sequelize.query(
-    "UPDATE farms SET home_county = '' WHERE home_county IS NULL"
-  );
-
   await queryInterface.changeColumn('farms', 'home_county', {
     type: DataTypes.STRING,
     allowNull: false,
