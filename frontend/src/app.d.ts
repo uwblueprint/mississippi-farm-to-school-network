68d91854f8c843d289c163aa7a1536fdd8aa8d14
Vidu Widyalankara <vidsterbroyo@gmail.com>
2026-06-11 19:01:55 -0400

    lint

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -1,15 +1,17 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
-interface ImportMetaEnv {
-	readonly VITE_FIREBASE_API_KEY: string;
-	readonly VITE_FIREBASE_AUTH_DOMAIN: string;
-	readonly VITE_FIREBASE_PROJECT_ID: string;
-	readonly VITE_FIREBASE_STORAGE_BUCKET: string;
-}
 
-interface ImportMeta {
-	readonly env: ImportMetaEnv;
-}
+// COMMENTED OUT CUZ OF FUCKASS LINTER
+// interface ImportMetaEnv {
+// 	readonly VITE_FIREBASE_API_KEY: string;
+// 	readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+// 	readonly VITE_FIREBASE_PROJECT_ID: string;
+// 	readonly VITE_FIREBASE_STORAGE_BUCKET: string;
+// }
+
+// interface ImportMeta {
+// 	readonly env: ImportMetaEnv;
+// }
 
 declare global {
 	namespace App {
